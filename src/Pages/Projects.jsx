1a2ea6cca78e24dae5b6352b41c98ee8f043d7e3
Gideon Cyril <114603633+IamUnike@users.noreply.github.com>
2023-07-09 16:56:03 +0100

    Update Projects.jsx

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,12 +1,12 @@
 const Projects = ({projects}) => {
     return ( 
         <div id="projects">
-            <section className="w-3/4 min-h-screen mx-auto mt-10 grid grid-cols-1 md:grid-col md:grid-cols-3 gap-4">
+            <section className="w-5/6 min-h-screen mx-auto mt-10 grid grid-cols-1 md:grid-col md:grid-cols-3 gap-4">
                 
                 {projects.map((project, index) => (
                     <div key={index} className="border border-secondary-200 flex flex-col justify-center gap-6  h-72 rounded-lg bg-secondary-200 overflow-hidden bg-opacity-30 text-white" data-aos='slide-right' data-aos-duration='700'>
                         
-                        <div className="h-32 w-3/4  rounded-md mx-auto overflow-hidden" data-aos='zoom-out' data-aos-duration='700' data-aos-delay='300'>
+                        <div className="h-32 w-4/5  rounded-md mx-auto overflow-hidden" data-aos='zoom-out' data-aos-duration='700' data-aos-delay='300'>
                             <img src={project.image} loading='lazy' alt="jobar cv project" className='w-full h-full object-contain' />
                         </div>
 

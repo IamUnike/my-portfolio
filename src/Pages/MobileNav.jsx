c7e9417082ed6696f236d2b2511e0fa1f05989c2
Gideon Cyril <114603633+IamUnike@users.noreply.github.com>
2023-07-09 16:58:01 +0100

    Update MobileNav.jsx

--- src/Pages/MobileNav.jsx
+++ src/Pages/MobileNav.jsx
@@ -12,22 +12,22 @@ import { motion } from "framer-motion"
 const MobileNav = ({mobileNav, setMobileNav}) => {
     return ( 
         <motion.nav 
-            className='text-white h-screen w-full top-0 z-80 fixed bg-black flex flex-col items-center justify-between pb-6 pt-3.5'
+            className='text-white h-screen w-full top-0 z-80 fixed bg-black flex flex-col items-center justify-around pb-6 pt-3.5'
             initial={{scale: 0}} animate={{scale: [0, 2, .7, 1]}} transition={{duration: 1, ease:'easeIn'}}  exit={{scale: 0, opacity:0}} >
                 
-                <div className='bg-secondary-200 rounded-md p-2 flex justify-between w-4/5  items-center'>
+                <div className='bg-secondary-200 rounded-md p-2 flex justify-between w-5/6  items-center'>
                     <p className='text-xs font-poppins'> Gideon Cyril </p>
                     
                     <i className='fa fa-close text-red-600 text-xl cursor-pointer' onClick={ () => setMobileNav(!mobileNav)}></i>
                 </div>
 
-                <div className='p-4 w-4/5 flex justify-center items-center gap-4'>
+                <div className='p-4 w-5/6 flex justify-center items-center gap-4'>
                    <a href={resume} download className='bg-purple-700 text-white text-sm rounded-md p-2 hover:bg-transparent border-2 border-purple-700 font-poppins duration-300 cursor-pointer'> My Resume </a>
 
                   <a href='mailto:gideontech21@gmail.com' className='bg-white text-sm text-purple-700 rounded-md p-2 hover:bg-transparent border-2 border-white font-poppins duration-300'> Contact Me </a> 
                 </div>
 
-                <div className='rounded-md p-4 w-4/5 flex justify-center items-center gap-3 overflow-auto bg-secondary-200 bg-opacity-50'>
+                <div className='rounded-md p-4 w-5/6 flex justify-center items-center gap-3 overflow-auto bg-secondary-200 bg-opacity-50'>
                     <p className='text-xs font-poppins transform rotate-90'> Skillset  </p>
                     
                     <img src={reactImg} alt="react" className='skill-img' loading='lazy' />

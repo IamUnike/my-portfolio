0e4574aa8f8b311a0f30f9a6c17d0939c48b80dd
Gideon Cyril <gideontech21@gmail.com>
2023-07-11 14:04:35 +0100

    a

--- src/Pages/MobileNav.jsx
+++ src/Pages/MobileNav.jsx
@@ -12,7 +12,7 @@ import { motion } from "framer-motion"
 const MobileNav = ({mobileNav, setMobileNav}) => {
     return ( 
         <motion.nav 
-            className='text-white h-screen w-full top-0 z-80 fixed bg-black flex flex-col items-center justify-between pb-8 pt-3.5'
+            className='text-white h-screen w-full top-0 z-80 fixed bg-black flex flex-col items-center justify-between pt-3.5'
             initial={{scale: 0}} animate={{scale: [0, 1.5, 1]}} transition={{duration: 1, ease:'easeIn'}}  exit={{scale: 0, opacity:0}} >
                 
                 <div className='bg-secondary-200 rounded-md p-2 flex justify-between w-5/6  items-center'>
@@ -27,7 +27,7 @@ const MobileNav = ({mobileNav, setMobileNav}) => {
                   <a href='mailto:gideontech21@gmail.com' className='bg-white text-sm text-purple-700 rounded-md p-2 hover:bg-transparent border-2 border-white font-poppins duration-300'> Contact Me </a> 
                 </div>
 
-                <div className='rounded-md p-4 w-5/6 flex justify-center items-center gap-3 overflow-auto bg-secondary-200 bg-opacity-50 mb-4'>
+                <div className='rounded-md p-4 w-5/6 flex justify-center items-center gap-3 overflow-auto bg-secondary-200 bg-opacity-50 mb-14'>
                     <p className='text-xs font-poppins transform rotate-90'> Skillset  </p>
                     
                     <img src={reactImg} alt="react" className='skill-img' loading='lazy' />
